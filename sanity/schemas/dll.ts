0b9f920e21b085d8abd020a3fbc11d135da13321
million23 <chavezgerald23@gmail.com>
2023-05-05 14:10:57 +0800

    updated category for dll

--- sanity/schemas/dll.ts
+++ sanity/schemas/dll.ts
@@ -9,6 +9,17 @@ export default {
 			type: "string",
 		},
 		{
+			name: "category",
+			title: "Category",
+			type: "string",
+			options: {
+				list: [
+					{ title: "Core Subjects", value: "core-subjects" },
+					{ title: "Specialized Subjects", value: "specialized-subjects" },
+				],
+			},
+		},
+		{
 			name: "file",
 			title: "File",
 			type: "file",
